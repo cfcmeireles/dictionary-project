2f444bfb47f96de74c979052635514f867358da4
cmeireles1 <carinacmeireles@gmail.com>
2022-05-29 03:09:49 +0100

    Updated Examples.js

--- src/Examples.js
+++ src/Examples.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Examples(props) {
-  if (props.example) {
+  if (props.example && props.example.length > 0) {
     return (
       <div className="Examples">
         {props.example.map(function (example, index) {
