a1ab1e4ae3dc7e4b3c86c1e1b1731d225a58318d
cmeireles1 <carinacmeireles@gmail.com>
2022-06-18 20:39:35 +0100

    removed listen option for arrays without audio, styled photos

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,12 +2,23 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
-        Listen
-      </a>
-      <span className="text">{props.phonetic.text}</span>
-    </div>
-  );
+  const audio = props.phonetic.audio;
+
+  if (audio) {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank">
+          Listen
+        </a>
+        <span className="text">{props.phonetic.text}</span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Phonetic">
+        {" "}
+        <span className="text">{props.phonetic.text}</span>
+      </div>
+    );
+  }
 }
